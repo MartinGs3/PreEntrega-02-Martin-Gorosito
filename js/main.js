c4f99e3efa999733ec1d6192ccdd4bb431f29b0e
Martin Gorosito <martinrc03@outlook.com>
2024-04-07 17:37:02 -0300

    guardado

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
-const titulos = document.getElementsByClassName("h1");
-for (let i = 0; i < titulos.length; i++) {
-    const titulo = titulos[i];
-    titulo.innerText += 'Forumulario';
-}
+
 
 document.getElementById("miFormulario").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const nombre = document.getElementById("nombre").value;
-    const email = document.getElementById("email").value;
+
+    const Nombres = document.getElementById("nombre").value;
+    const Emails = document.getElementById("email").value;
+    
+    function confirmacion(){
+        const imagenConfirmacion = document.createElement("img");
+    imagenConfirmacion.src = "https://definicion.de/wp-content/uploads/2011/04/correspondencia.jpg";
+    imagenConfirmacion.classList.add("enviado");
+    imagenConfirmacion.alt = "confirmacion";
+    document.getElementById("caja").appendChild(imagenConfirmacion);
+    setTimeout(function() {
+        imagenConfirmacion.remove();
+    }, 8000);
+    }
+
+    function negacion(){
+        const imagenNegacion = document.createElement("img");
+        imagenNegacion.src = "https://thumbs.dreamstime.com/z/no-send-sign-isolated-white-background-vector-illustration-193341489.jpg";
+        imagenNegacion.alt = "negacion";
+        imagenNegacion.classList.add("noEnviado");
+        document.getElementById("caja").appendChild(imagenNegacion);
+        setTimeout(function() {
+            imagenNegacion.remove();
+        }, 8000);
+    }    
+    
 
     const datosUsuario = {
-        nombre: nombre,
-        email: email
+        Nombre: Nombres,
+        Email: Emails
     };
 
     const datosUsuarioJSON = JSON.stringify(datosUsuario);
 
     localStorage.setItem("usuario", datosUsuarioJSON);
 
-    document.getElementById("miFormulario").reset();
+    Nombres === "" || Emails === "" ? negacion() : confirmacion()
+
+   document.getElementById("miFormulario").reset();
 });
